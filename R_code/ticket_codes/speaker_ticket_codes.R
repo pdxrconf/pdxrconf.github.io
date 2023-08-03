8933d893588e6cea394bc2e15f727c17c98b6ed1
Kimberly Dill-McFarland <kdillmcfarland@gmail.com>
2023-08-03 10:28:01 -0700

    Update speaker_ticket_codes.R

--- R_code/ticket_codes/speaker_ticket_codes.R
+++ R_code/ticket_codes/speaker_ticket_codes.R
@@ -14,16 +14,16 @@ write.table(paste0(prefix, IDs), row.names = FALSE, col.names = FALSE, sep=",",
 #### SCHOLARSHIP ####
 
 set.seed(55)
-IDs <- sample(1000:9999, 8, replace=FALSE)
+IDs <- sample(1000:9999, 20, replace=FALSE)
 
 prefix <- "2023CRC_"
-s1 <- paste0(prefix, IDs[1:3])
+s1 <- paste0(prefix, IDs[c(1:3,9:10)])
 
 write.table(s1, row.names = FALSE, col.names = FALSE, sep=",",
             "2023_scholar_codes.csv")
 
 prefix <- "2023VIRTUAL_"
-s2 <- paste0(prefix, IDs[4:8])
+s2 <- paste0(prefix, IDs[c(4:8,11:length(IDs))])
 
 write.table(s2, row.names = FALSE, col.names = FALSE, sep=",",
             "2023_scholar_virtual_codes.csv")
